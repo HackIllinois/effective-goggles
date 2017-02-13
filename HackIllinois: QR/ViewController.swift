84da88f9b8ce54f2f96226b34df9881bbba37ff8
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-02-12 19:19:16 -0600

    Initial project commit

---
+++ HackIllinois: QR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackIllinois: QR
+//
+//  Created by Yasha Mostofi on 2/12/17.
+//  Copyright © 2017 Yasha Mostofi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
