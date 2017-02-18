9ce84221c54b7f62815a2777623b473fb94435a9
Yasha Mostofi <yasha.mostofi@gmail.com>
2017-02-18 01:16:32 -0600

    Users can now create Universally Tracked events

--- HackIllinois: QR/AddViewController.swift
+++ HackIllinois: QR/AddViewController.swift
@@ -7,37 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class AddViewController: BaseViewController {
-
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var durationField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "session") != nil
+        {
+            self.login_session = preferences.object(forKey: "session") as! String
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
+  
     @IBAction func cancelButton(_ sender: Any) {
         print("cancel")
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func saveButton(_ sender: Any) {
         print("save")
-        self.dismiss(animated: true, completion: nil)
+        let role = self.check_permissions(key: self.login_session)
+        print(role)
+        if role != "ADMIN" {
+            self.dismiss(animated: true)
+            return
+        }
+        let headers: HTTPHeaders = [
+            "Authorization": self.login_session
+        ]
+        let parameters: Parameters = [
+            "name": nameField.text!,
+            "duration": durationField.text!
+        ]
+        print(parameters)
+        Alamofire.request("https://api.hackillinois.org/v1/tracking", method: .post,
+                          parameters: parameters, encoding: JSONEncoding.default,
+                          headers: headers).validate().responseJSON { [weak self] response in
+                            print(response)
+                            switch response.result {
+                            case .success:
+                                self?.dismiss(animated: true)
+                            case .failure:
+//                                self?.displayAlert(title: "ERROR!", message: "Could not create new event")
+                                self?.dismiss(animated: true)
+                            }
+        }
     }
 }
